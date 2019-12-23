dee453201efed6a2a85fb340f4e0b3715e348d36
Sidney NiBo <snibo13@gmail.com>
2019-12-23 14:06:40 -0600

    Initial commit

---
+++ colors.js
@@ -0,0 +1,32 @@
+//Find a color complementary to the color parameter c
+function getComplementary(c) {
+
+}
+
+//Find a monochromatic color for the color parameter c
+function getMonochromatic(c) {
+
+}
+
+//Find analagous colors for the color parameter c
+function getAnalagous(c) {
+
+}
+
+//Find a pair of triadic colors for hte color parameter c
+function getTriadic(c) {
+
+}
+
+//Find a set of tetradic colors for the color parameter c
+function getTetradic(c) {
+
+}
+
+//Determine if a color is warm or cool
+function colorWarmth(c) {
+
+}
+
+//Convert a color to it's color wheel equivalent
+
